9099098ddf69574a15ae85fcd58dd1bad4b7a12c
Azamat <azamatdeveloper3@gmail.com>
2024-02-12 16:58:25 +0500

    Added timer progress bar dependency...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "android.azadev.quizzical"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "android.azadev.quizzical"
@@ -46,4 +46,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.github.antonKozyriatskyi:CircularProgressIndicator:1.3.0")
 }
