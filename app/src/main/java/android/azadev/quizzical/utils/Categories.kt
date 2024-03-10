f5b117cf997807923047ec77ca17ec07469629d9
Azamat <azamatdeveloper3@gmail.com>
2024-03-10 18:48:18 +0500

    Added quiz categories ids and preferences keys to Constants.kt. Created Categories.kt enum.

---
+++ app/src/main/java/android/azadev/quizzical/utils/Categories.kt
@@ -0,0 +1,13 @@
+package android.azadev.quizzical.utils
+
+/**
+ * Created by : Azamat Kalmurzayev
+ * Date : 3/10/2024
+ */
+
+enum class Categories {
+    Geography,
+    History,
+    Math,
+    Sport
+}
--- app/src/main/java/android/azadev/quizzical/utils/Constants.kt
+++ app/src/main/java/android/azadev/quizzical/utils/Constants.kt
@@ -1,5 +1,9 @@
 package android.azadev.quizzical.utils
 
+import android.azadev.quizzical.R
+import android.content.Context
+import androidx.core.content.ContextCompat.getString
+
 /**
  * Created by : Azamat Kalmurzayev
  * Date : 2/16/2024
@@ -11,12 +15,45 @@ object Constants {
 
     const val QUESTIONS_AMOUNT = 20
 
-    val SCIENCE_CATEGORY_ID = (17..20).random()
+    const val HISTORY_ID = 23
 
-    const val GENERAL_CATEGORY_ID = 9
+    const val GEOGRAPHY_ID = 22
 
-    val ENTERTAINMENT_CATEGORY_ID = (11..16).random()
+    const val MATH_ID = 19
+
+    const val SPORT_ID = 21
+
+    const val GENERAL_KNOWLEDGE = 9
 
     const val TOTAL_SECONDS = 20
+
+    const val PREFS_SCORE_ID = "scoreId"
+    const val PREFS_USER_ID = "userId"
+    const val PREFS_IS_HAVE = "isHave"
+
+    fun getMonth(month: Int, context: Context): String {
+        return when (month) {
+            1 -> getString(context, R.string.month_january)
+            2 -> getString(context, R.string.month_february)
+            3 -> getString(context, R.string.month_march)
+            4 -> getString(context, R.string.month_april)
+            5 -> getString(context, R.string.month_may)
+            6 -> getString(context, R.string.month_june)
+            7 -> getString(context, R.string.month_july)
+            8 -> getString(context, R.string.month_august)
+            9 -> getString(context, R.string.month_september)
+            10 -> getString(context, R.string.month_october)
+            11 -> getString(context, R.string.month_november)
+            12 -> getString(context, R.string.month_december)
+            else -> {
+                getString(context, R.string.month_january)
+            }
+        }
+    }
+
+    fun makeDateToString(day: Int, month: Int, year: Int, context: Context): String {
+        return "${getMonth(month, context)} $day $year"
+    }
+
 
 }
