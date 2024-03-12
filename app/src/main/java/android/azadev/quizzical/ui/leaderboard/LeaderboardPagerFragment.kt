9a034f576c6fe25dc7d17c3db1324c9ca565350f
Azamat <azamatdeveloper3@gmail.com>
2024-03-12 23:16:57 +0500

    Created LeaderboardUserAdapter.kt and LeaderboardViewModel.kt and implemented in LeaderboardPagerFragment.kt. Added no_data textview in fragment_leaderboard_pager.xml.

--- app/src/main/java/android/azadev/quizzical/ui/leaderboard/LeaderboardPagerFragment.kt
+++ app/src/main/java/android/azadev/quizzical/ui/leaderboard/LeaderboardPagerFragment.kt
@@ -1,24 +1,62 @@
 package android.azadev.quizzical.ui.leaderboard
 
 import android.azadev.quizzical.R
+import android.azadev.quizzical.data.local.entity.UserAndScore
 import android.azadev.quizzical.databinding.FragmentLeaderboardPagerBinding
+import android.azadev.quizzical.ui.leaderboard.adapter.LeaderboardUserAdapter
+import android.azadev.quizzical.ui.leaderboard.viewmodel.LeaderboardViewModel
+import android.azadev.quizzical.utils.UIExtensions.inVisible
+import android.azadev.quizzical.utils.UIExtensions.visible
 import android.os.Bundle
 import android.view.View
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 /**
  * Created by : Azamat Kalmurzayev
  * Date : 3/7/2024
  */
 
+@AndroidEntryPoint
 class LeaderboardPagerFragment : Fragment(R.layout.fragment_leaderboard_pager) {
     private var _binding: FragmentLeaderboardPagerBinding? = null
     private val binding get() = _binding!!
+    private lateinit var adapter: LeaderboardUserAdapter
+
+    private val viewModel: LeaderboardViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentLeaderboardPagerBinding.bind(view)
 
+        val category = arguments?.getString(CATEGORY_NAME)
+
+        viewModel.userAndScore
+            .flowWithLifecycle(viewLifecycleOwner.lifecycle)
+            .distinctUntilChanged()
+            .onEach { users ->
+                configureUserAdapter(users)
+            }.launchIn(viewLifecycleOwner.lifecycleScope)
+
+        category?.let { viewModel.getAllUsers(it) }
+
+    }
+
+    private fun configureUserAdapter(userAndScores: List<UserAndScore>) {
+        if (userAndScores.isEmpty()) {
+            binding.tvNoData.visible()
+        } else {
+            binding.tvNoData.inVisible()
+            adapter = LeaderboardUserAdapter(userAndScores)
+            binding.rvUser.adapter = adapter
+        }
     }
 
     override fun onDestroyView() {
@@ -27,8 +65,10 @@ class LeaderboardPagerFragment : Fragment(R.layout.fragment_leaderboard_pager) {
     }
 
     companion object {
-        fun newInstance(): Fragment {
+        const val CATEGORY_NAME = "category"
+        fun newInstance(category: String): Fragment {
             val fragment = LeaderboardPagerFragment()
+            fragment.arguments = bundleOf(CATEGORY_NAME to category)
             return fragment
         }
     }
